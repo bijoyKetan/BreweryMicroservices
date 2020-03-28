7150e62c5d26b0fe17a11c9bb39c1e6e2999930e
Raqeebul Ketan <bijoy.ketan@gmail.com>
2020-03-28 11:35:51 -0400

    Implemented the getBeerId controller.

--- brewery/src/main/java/com/bijoyketan/brewery/service/BeerServiceImpl.java
+++ brewery/src/main/java/com/bijoyketan/brewery/service/BeerServiceImpl.java
@@ -1,9 +1,11 @@
 package com.bijoyketan.brewery.service;
 
 import com.bijoyketan.brewery.web.model.BeerDto;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public BeerDto getBeerbyID(UUID beerID) {
--- brewery/src/main/java/com/bijoyketan/brewery/web/controller/BeerController.java
+++ brewery/src/main/java/com/bijoyketan/brewery/web/controller/BeerController.java
@@ -2,28 +2,41 @@ package com.bijoyketan.brewery.web.controller;
 
 import com.bijoyketan.brewery.service.BeerService;
 import com.bijoyketan.brewery.web.model.BeerDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/beer")
 public class BeerController {
 
     private final BeerService beerService;
+    private Logger log = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
-    public BeerController (BeerService beerService){
+    public BeerController(BeerService beerService) {
         this.beerService = beerService;
     }
 
     //Get beer by pathvariable
-    //TODO - Complete this.
-    @GetMapping
-    public ResponseEntity<BeerDto> getBeerByID(){
-        return null;
+    @GetMapping(value = "/{beerID}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> getBeerByID(HttpServletRequest request,  @PathVariable String beerID) {
+        try{
+            return new ResponseEntity<>(beerService.getBeerbyID(UUID.fromString(beerID)), HttpStatus.OK);
+        } catch (Exception e){
+            e.printStackTrace();
+            log.debug(request.getParameter("json"));
+            return new ResponseEntity<>("Error in getBeerByID request. Check the payload", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
-
 }
